670b3b65823d8d3e6c0b3aa55b89a8f5f8623013
Anton <jnashicq@gmail.com>
2021-09-14 16:55:40 +0300

    Change app name to Mysterium Launcher

--- make_dmg.sh
+++ make_dmg.sh
@@ -1,8 +1,8 @@
 xcodebuild ARCHS=x86_64 ONLY_ACTIVE_ARCH=NO clean build -destination 'platform=macOS' -project "launcher.xcodeproj"
 
-rm -rf build/Release/Launcher.app.dSYM
-mv build/Release/ build/MystLauncher
-ln -s /Applications build/MystLauncher/Applications
+rm -rf build/Release/Mysterium\ Launcher.app.dSYM
+mv build/Release/ build/MysteriumLauncher
+ln -s /Applications build/MysteriumLauncher/Applications
 
-hdiutil create -format UDZO -srcfolder build/MystLauncher myst_launcher_macos_amd64.dmg
+hdiutil create -format UDZO -srcfolder build/MysteriumLauncher mysterium_launcher_macos_amd64.dmg
 
