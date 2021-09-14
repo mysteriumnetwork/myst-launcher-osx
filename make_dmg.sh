5936eb25805b5c2012be14e11c8b2fd1d238f6c1
Anton <jnashicq@gmail.com>
2021-09-14 12:36:20 +0300

    Fix dmg name

--- make_dmg.sh
+++ make_dmg.sh
@@ -1,8 +1,3 @@
-#sudo xcode-select --switch /Library/Developer/CommandLineTools
-#sudo xcode-select -s /Users/macmini/Downloads/Xcode.app/Contents/Developer
-
 xcodebuild ARCHS=x86_64 ONLY_ACTIVE_ARCH=NO clean build -destination 'platform=macOS' -project "launcher.xcodeproj"
-hdiutil create -format UDZO -srcfolder build/Release myst_launcher.dmg
+hdiutil create -format UDZO -srcfolder build/Release myst_launcher_macos_amd64.dmg
 
-#xcodebuild archive -archivePath archive/result.xcarchive -scheme launcher -configuration "Release" ARCHS="x86_64h" -destination 'platform=macOS' -project "launcher.xcodeproj"
-#xcodebuild archive -archivePath archive/result.xcarchive -exportArchive -exportOptionsPlist exportOptionsAdHoc.plist -exportPath app
