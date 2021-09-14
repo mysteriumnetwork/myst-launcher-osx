5682e6695b7a3d99f466387796ba855f757da799
Anton Litvinov <jnashicq@gmail.com>
2021-09-14 09:51:39 +0300

    Add automatic builds

--- make_dmg.sh
+++ make_dmg.sh
@@ -1,7 +1,7 @@
 #sudo xcode-select --switch /Library/Developer/CommandLineTools
 #sudo xcode-select -s /Users/macmini/Downloads/Xcode.app/Contents/Developer
 
-xcodebuild clean build -destination 'platform=macOS' -project "launcher.xcodeproj"
+xcodebuild ARCHS=x86_64 ONLY_ACTIVE_ARCH=NO clean build -destination 'platform=macOS' -project "launcher.xcodeproj"
 hdiutil create -format UDZO -srcfolder build/Release myst_launcher.dmg
 
 #xcodebuild archive -archivePath archive/result.xcarchive -scheme launcher -configuration "Release" ARCHS="x86_64h" -destination 'platform=macOS' -project "launcher.xcodeproj"
