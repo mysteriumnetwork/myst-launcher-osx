a2bed3824381efede56aaac7dfd1c166d8230802
Anton <jnashicq@gmail.com>
2021-09-14 16:10:11 +0300

    Make dmg able to install the app

--- make_dmg.sh
+++ make_dmg.sh
@@ -1,3 +1,8 @@
 xcodebuild ARCHS=x86_64 ONLY_ACTIVE_ARCH=NO clean build -destination 'platform=macOS' -project "launcher.xcodeproj"
-hdiutil create -format UDZO -srcfolder build/Release myst_launcher_macos_amd64.dmg
+
+rm -rf build/Release/Launcher.app.dSYM
+mv build/Release/ build/MystLauncher
+ln -s /Applications build/MystLauncher/Applications
+
+hdiutil create -format UDZO -srcfolder build/MystLauncher myst_launcher_macos_amd64.dmg
 
