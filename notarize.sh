f20fd0520b31e1c9c9fa4db412f111e0c92c0573
Anton <jnashicq@gmail.com>
2021-09-21 19:34:42 +0300

    Notarize

--- notarize.sh
+++ notarize.sh
@@ -1,6 +1,4 @@
-
-
-
+#!/bin/sh
 
 # Submit the dmg and get REQUEST_UUID
 SUBMISSION_INFO=$(xcrun altool --notarize-app --primary-bundle-id=${PRIMARY_BUNDLE_ID} -u ${APPLE_ID} -p ${APP_SPECIFIC_PASSWORD} --file ${TARGET_BINARY} 2>&1) ;
@@ -17,10 +15,10 @@ if [ -z "${REQUEST_UUID}" ]; then
 fi
 
 # Wait for "Package Approved"
-timeout 10m bash -c "
-    while ! xcrun altool --notarization-info ${REQUEST_UUID} --username ${APPLE_ID} --password ${APP_SPECIFIC_PASSWORD} --output-format xml | grep -q 'Package Approved' ; do
-        sleep 60;
-    done"
+while ! xcrun altool --notarization-info ${REQUEST_UUID} --username ${APPLE_ID} --password ${APP_SPECIFIC_PASSWORD} --output-format xml | grep -q 'Package Approved' ; do
+    sleep 60;
+done
 
 echo "Package Approved: REQUEST_UUID=$REQUEST_UUID can be accessed with this query: xcrun altool --notarization-info $REQUEST_UUID --username ${APPLE_ID} --output-format xml --password app_specific_password"
 
+xcrun stapler staple ${TARGET_BINARY}
