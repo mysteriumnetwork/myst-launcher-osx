c4a8a3a7c2c7bcca1effd05cab7c3a270ed0dad6
Anton <jnashicq@gmail.com>
2021-09-22 14:55:31 +0300

    Notarize

--- make_build.sh
+++ make_build.sh
@@ -1,8 +1,6 @@
-xcodebuild ARCHS=x86_64 ONLY_ACTIVE_ARCH=NO clean build -destination 'platform=macOS' -project "launcher.xcodeproj"
+xcodebuild ARCHS=x86_64 ONLY_ACTIVE_ARCH=NO -configuration Release clean build -destination 'platform=macOS' -project "launcher.xcodeproj"
 
-rm -rf build/Release/Mysterium\ Launcher.app.dSYM
 mv build/Release/ build/MysteriumLauncher
 ln -s /Applications build/MysteriumLauncher/Applications
 
 #hdiutil create -format UDZO -srcfolder build/MysteriumLauncher mysterium_launcher_macos_amd64.dmg
-
