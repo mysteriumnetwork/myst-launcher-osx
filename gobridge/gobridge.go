f233fd3982722d061cdcb121399d46ec69bd04ce
Anton Litvinov <jnashicq@gmail.com>
2021-12-29 22:22:39 +0300

    Add universal binary support (#16)

--- gobridge/gobridge.go
+++ gobridge/gobridge.go
@@ -79,6 +79,7 @@ func GoStart() {
 	mod.UIBus.Subscribe("launcher-update", func() {
 		C.macSendOpenDialogue(C.int(1))
 	})
+	mod.DuplicateLogToConsole = true
 	log.SetOutput(ap)
 
 	mod.UIBus.Subscribe("model-change", sendState)
@@ -91,6 +92,7 @@ func GoStart() {
 	ap.WaitGroup.Add(1)
 	go ap.SuperviseDockerNode()
 	go ap.CheckLauncherUpdates(gitHubOrg, gitHubRepo)
+
 }
 
 func sendState() {
@@ -104,7 +106,7 @@ func sendState() {
 	st.containerRunning = C.int(mod.StateContainer)
 	st.networkCaption = C.CString(mod.Config.GetNetworkCaption())
 
-	// instllation state
+	// installation state
 	st.checkVirt = C.int(mod.CheckVirt)
 	st.checkDocker = C.int(mod.CheckDocker)
 	st.downloadFiles = C.int(mod.DownloadFiles)
@@ -185,7 +187,7 @@ func GoSetNetworkConfig(s *C.NSConfig) {
 //export GoUpdateToMainnet
 func GoUpdateToMainnet() {
 	mod.UpdateToMainnet()
-    sendConfig()
+	sendConfig()
 }
 
 // required by runtime
--- gobridge/mk.sh
+++ gobridge/mk.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
-clang -c -x objective-c interface.m -o interface.o
-CC=clang go build -o gobridge.dylib -buildmode=c-shared *.go
+#arm64
+clang -arch arm64 -c -x objective-c interface.m -o interface.o
+CGO_ENABLED=1 GOOS=darwin GOARCH=arm64 CC=clang go build -ldflags "-s -w" -o gobridge_arm64.dylib -buildmode=c-shared *.go
+
+#amd64
+clang -arch x86_64 -c -x objective-c interface.m -o interface.o
+CGO_ENABLED=1 GOOS=darwin GOARCH=amd64 CC=clang go build -ldflags "-s -w" -o gobridge_amd64.dylib -buildmode=c-shared *.go
+
+# merge 2 into 1
+lipo -create -output gobridge.dylib gobridge_amd64.dylib gobridge_arm64.dylib
 
 # set proper LC_ID_DYLIB. @rpath - means a library could be found in runtime path.
 # otherwise app (dynamic linker) wont be be able to find it during start.
